26bff4b4d484920f73ff592bfe5e62d9709c3c98
SSABOODA <ssaboo92@gmail.com>
2023-12-11 17:31:24 +0900

    profile 하단 tab man 적용

--- PictureFlow/Controllers/Profile/ProfileViewController.swift
+++ PictureFlow/Controllers/Profile/ProfileViewController.swift
@@ -6,9 +6,128 @@
 //
 
 import UIKit
+import Tabman
+import Pageboy
 
 final class ProfileViewController: UIViewController {
+    let mainView = ProfileView()
+    
+    override func loadView() {
+        view = mainView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNavigationBar()
+        makeContainerViewController()
+    }
+    
+    func makeContainerViewController() {
+        let childViewController = ChildViewController()
+        addChild(childViewController)
+        view.addSubview(childViewController.view)
+        childViewController.didMove(toParent: self)
+        
+        childViewController.view.snp.makeConstraints { make in
+            make.top.equalTo(mainView.profileInfoView.snp.bottom)
+            make.horizontalEdges.bottom.equalTo(view)
+        }
     }
 }
+
+extension ProfileViewController {
+    private func configureNavigationBar() {
+        let settingButton = UIBarButtonItem(
+            image: UIImage(named: "setting"),
+            style: .plain,
+            target: self,
+            action: #selector(settingButtonClicked)
+        )
+        
+        let profileUpdateButton = UIBarButtonItem(
+            image: UIImage(named: "profile-update"),
+            style: .plain,
+            target: self,
+            action: #selector(settingButtonClicked)
+        )
+        
+        navigationItem.rightBarButtonItems = [
+            settingButton,
+            profileUpdateButton,
+        ]
+        navigationItem.rightBarButtonItems?.forEach({ item in
+            item.tintColor = UIColor(resource: .tint)
+        })
+    }
+    
+    @objc func settingButtonClicked() {
+        print("setting button did tap")
+    }
+}
+
+
+class ChildViewController: TabmanViewController, PageboyViewControllerDataSource, TMBarDataSource {
+    func barItem(for bar: Tabman.TMBar, at index: Int) -> Tabman.TMBarItemable {
+        let item = TMBarItem(title: "")
+        item.title = "Page \(index)"
+        item.image = UIImage(named: "image.png")
+        // ↑↑ 이미지는 이따가 탭바 형식으로 보여줄 때 사용할 것이니 "이미지가 왜 있지?" 하지말고 넘어가주세요.
+        
+        return item
+    }
+    
+    func numberOfViewControllers(in pageboyViewController: Pageboy.PageboyViewController) -> Int {
+        return viewControllers.count
+    }
+    
+    func viewController(for pageboyViewController: Pageboy.PageboyViewController, at index: Pageboy.PageboyViewController.PageIndex) -> UIViewController? {
+        return viewControllers[index]
+    }
+    
+    func defaultPage(for pageboyViewController: Pageboy.PageboyViewController) -> Pageboy.PageboyViewController.Page? {
+        return nil
+    }
+    
+    private var viewControllers: Array<UIViewController> = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(resource: .background)
+        view.layer.cornerRadius = 20
+        view.clipsToBounds = true
+        
+        configureTabViewController()
+    }
+    
+    private func configureTabViewController() {
+        let vc1 = ViewController1()
+        let vc2 = ViewController2()
+        viewControllers.append(vc1)
+        viewControllers.append(vc2)
+        
+        self.dataSource = self
+        
+        let bar = TMBar.ButtonBar()
+        bar.layout.transitionStyle = .snap // Customize
+        
+        // Add to view
+        addBar(bar, dataSource: self, at: .top)
+    }
+}
+
+class ViewController1: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+}
+
+
+class ViewController2: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
+
+
---
+++ PictureFlow/Views/Profile/ProfileView.swift
@@ -0,0 +1,148 @@
+//
+//  ProfileView.swift
+//  PictureFlow
+//
+//  Created by 한성봉 on 12/11/23.
+//
+
+import UIKit
+
+final class ProfileView: UIView {
+    let profileInfoView = {
+        let view = UIView()
+        return view
+    }()
+    
+    let nickNameLabel = {
+        let label = UILabel()
+        label.text = "ssabooda"
+        label.font = .boldSystemFont(ofSize: 30)
+        return label
+    }()
+    
+    let profileImageView = {
+        let view = UIImageView()
+        view.image = UIImage(systemName: "person")
+        view.contentMode = .scaleToFill
+        return view
+    }()
+    
+    let followerUserProfileImageView1 = {
+        let view = UIImageView()
+        view.image = UIImage(systemName: "person")
+        return view
+    }()
+    
+    let followerUserProfileImageView2 = {
+        let view = UIImageView()
+        view.image = UIImage(systemName: "person")
+        return view
+    }()
+    
+    let followerUserProfileImageView3 = {
+        let view = UIImageView()
+        view.image = UIImage(systemName: "person")
+        return view
+    }()
+
+    let followerLabel = {
+        let label = UILabel()
+        label.textColor = .lightGray
+        label.text = "팔로워 9명"
+        return label
+    }()
+    
+    let bottomView = {
+        let view = UIView()
+        return view
+    }()
+
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .black
+        configureHierarchy()
+        configureLayout()
+        
+        let urlString = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU1vlLWxAULVL6qo3QVjH9-c5KxHWJgk7U3eZWReHeig&s"
+        if let imageURL = URL(string: urlString) {
+            profileImageView.kf.setImage(with: imageURL)
+        }
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let profileImageViewList = [
+            followerUserProfileImageView1,
+            followerUserProfileImageView2,
+            followerUserProfileImageView3,
+            profileImageView
+        ]
+        
+        profileImageViewList.forEach { imageView in
+            imageView.layoutIfNeeded()
+            imageView.layer.cornerRadius = imageView.frame.width / 2
+            imageView.clipsToBounds = true
+        }
+    }
+    
+    private func configureHierarchy() {
+        addSubview(profileInfoView)
+        profileInfoView.addSubview(nickNameLabel)
+        profileInfoView.addSubview(profileImageView)
+        profileInfoView.addSubview(followerUserProfileImageView1)
+        profileInfoView.addSubview(followerUserProfileImageView2)
+        profileInfoView.addSubview(followerUserProfileImageView3)
+        profileInfoView.addSubview(followerLabel)
+        addSubview(bottomView)
+    }
+    
+    private func configureLayout() {
+        profileInfoView.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(safeAreaLayoutGuide)
+            make.height.equalToSuperview().multipliedBy(0.13)
+        }
+        
+        nickNameLabel.snp.makeConstraints { make in
+            make.top.equalTo(profileImageView.snp.top)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalTo(profileImageView.snp.leading).offset(-15)
+        }
+        
+        profileImageView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().offset(-20)
+            make.size.equalTo(80)
+        }
+
+        followerUserProfileImageView1.snp.makeConstraints { make in
+            make.bottom.equalTo(profileImageView.snp.bottom)
+            make.leading.equalTo(nickNameLabel.snp.leading)
+            make.size.equalTo(20)
+        }
+        
+        followerUserProfileImageView2.snp.makeConstraints { make in
+            make.top.equalTo(followerUserProfileImageView1.snp.top)
+            make.size.equalTo(20)
+            make.leading.equalTo(followerUserProfileImageView1.snp.leading).offset(10)
+        }
+        
+        followerUserProfileImageView3.snp.makeConstraints { make in
+            make.top.equalTo(followerUserProfileImageView1.snp.top)
+            make.size.equalTo(20)
+            make.leading.equalTo(followerUserProfileImageView2.snp.leading).offset(10)
+        }
+        
+        followerLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(followerUserProfileImageView3.snp.centerY)
+            make.leading.equalTo(followerUserProfileImageView3.snp.trailing).offset(10)
+        }
+        
+    }
+    
+}
